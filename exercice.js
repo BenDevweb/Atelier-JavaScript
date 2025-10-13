3b202986e9ef6244dcceb1b1bc1a728f1157c505
BenDevweb <btechinnov746@gmail.com>
2025-10-13 20:26:09 +0100

    la fonction age majeur

---
+++ exercice.js
@@ -0,0 +1,13 @@
+// Exercice  fonction age majeur
+
+function ageMajeur(age) {
+    if (age >= 18) {
+        return "Vous êtes majeur.";
+    } else {
+        return "Vous êtes mineur.";
+    }
+}
+
+
+console.log(ageMajeur(20)); 
+console.log(ageMajeur(16)); 
